d9554653d97a88d5ced08fe2e95880e2fe61bc7d
Bric66 <cherouard@CEDRIC-HEROUARD>
2022-10-14 12:06:43 +0200

    version1

---
+++ hey_mercedes.js
@@ -0,0 +1,15 @@
+if (process.env.COMMAND === "weather") {
+    console.log('40° today');
+}
+
+else if (process.env.COMMAND === "address") {
+    console.log('25 rue Camille Gili 66300 Thuir');
+}
+
+else if (process.env.COMMAND === "quoteday") {
+    console.log("You see in this world there's two kinds of people, my friend. Those with loaded guns, and those who dig. You dig.");
+}
+
+else {
+    console.log("perdu!;)");
+}
