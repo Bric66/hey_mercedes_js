98869cf8d55f46b5b92a84f7868e2fa60c790ee1
Bric66 <cherouard@CEDRIC-HEROUARD>
2022-10-14 16:18:14 +0200

    ex1 et ex2

--- hey_mercedes2/hey_mercedes2.js
+++ hey_mercedes2/hey_mercedes2.js
@@ -3,7 +3,7 @@ const command = process.env.COMMAND
 if (command === "weather") {
     require ('./weather.js');
 }else if (command === "address") {
-    console.log('25 rue Camille Gili 66300 Thuir');
+    require ('./address.js');
 }else if (command === "quoteday") {
     require ('./quoteday.js');
 }else {
